de6cbaf7975e2be9b39f6eb7f4b8a2d4c1e85db5
Damiaan Dufaux <damiaan@dpro.be>
2020-05-08 16:30:52 +0200

    Frame TCP chunks, use camera banks

--- example.js
+++ example.js
@@ -1,16 +1,21 @@
 const rp = require('./');
 
-rp.eventEmitter.on('control', function(tally, address) {
-	console.log(tally, address);
+rp.on(rp.eventTypes.sBus, function({channel, camera, ipSocket}) {
+	console.log(ipSocket.remoteAddress, channel, camera);
 });
 
-rp.eventEmitter.on(
-	'connection', function(sock) {
-		console.log(sock.remoteAddress, 'connected')
-	}
-);
+rp.on(rp.eventTypes.connection, function(sock) {
+	console.log(sock.remoteAddress, 'connected')
+});
 
-rp.setProgramTally()
+rp.on(rp.eventTypes.close, function(sock) {
+	console.log(sock.remoteAddress, 'disconnected')
+})
+
+rp.setTally("10.1.0.18", 1)
+rp.setTally("10.1.0.18", 2)
+rp.setTally("10.1.0.18", 4)
+rp.setTally("10.1.0.18", 8)
 
 //one liner
 //const rp=require('./');rp.eventEmitter.on('control',function(tally, address){console.log(tally,address);});rp.eventEmitter.on('connection', function(sock) {console.log(sock.remoteAddress, 'connected')});
--- index.js
+++ index.js
@@ -5,29 +5,79 @@ const events = require('events'),
 	net = require('net'),
 	dgram = require('dgram');
 
-module.exports = new events.EventEmitter();
+const rawMessage = Symbol('Raw message')
+const sBusMessage = Symbol('SBUS message')
+const errorMessage = Symbol('Error message')
+const connectionMessage = Symbol('Connection message')
+const closeMessage = Symbol('Close message')
 
 const listener = net.createServer();
 listener.listen(myPort);
 listener.on('connection', function(sock) {
-	module.exports.emit('connection', sock.remoteAddress);
+	module.exports.emit(connectionMessage, sock);
+
+	let inbox = Buffer.alloc(0)
 	
-	sock.on('data', function(data) {
-		var position = data.toString().search(/SBUS:[0-9]{2}:[0-9]{2}/g);
-		if (position !== -1) {
-			var cTally = parseInt(data.toString().slice(position+8, position+10))-50;
-			module.exports.emit('control', cTally, sock.remoteAddress); 
+	sock.on('data', deframe);
+
+	function deframe(data) {
+		let nextEndOfText = data.indexOf(3)
+		if (nextEndOfText == -1) {
+			inbox = Buffer.concat([inbox, data])
+		} else {
+			let nextStartOfText;
+			if (inbox.length > 0) {
+				parseMessage(Buffer.concat([inbox, data.slice(0, nextEndOfText)]))
+				inbox = Buffer.alloc(0)
+				nextStartOfText = data.indexOf(2, nextEndOfText)
+			} else {
+				nextStartOfText = data.indexOf(2)
+			}
+
+			while (nextStartOfText != -1) {
+				nextEndOfText = data.indexOf(3, nextStartOfText)
+				if (nextEndOfText == -1) {
+					inbox = data.slice(nextStartOfText + 1)
+					nextStartOfText = -1
+				} else {
+					parseMessage(data.slice(nextStartOfText+1, nextEndOfText))
+					nextStartOfText = data.indexOf(2, nextEndOfText)
+				}
+			}
 		}
-	});
+	}
+
+	function parseMessage(message) {
+		module.exports.emit(rawMessage, message)
+		const messageName = message.toString('ascii', 0, 4)
+		if (messageName == "SBUS") {
+			const body = message.toString('ascii', 4)
+			const matches = body.match(/:([0-9]{2,}):([0-9]{2})/)
+			if (matches.length > 2) {
+				const channel = parseInt(matches[1])
+				const cameraId = parseInt(matches[2])
+				const zeroBasedCameraNumber = cameraId - 1
+				module.exports.emit(sBusMessage, {
+					channel,
+					camera: {
+						id: cameraId,
+						bank: Math.floor(zeroBasedCameraNumber / 49),
+						index: zeroBasedCameraNumber % 49
+					},
+					ipSocket: sock
+				})
+			}
+		}
+	}
 
 	sock.on('close', function() {
-		module.exports.emit('close', sock.remoteAddress);
+		module.exports.emit(closeMessage, sock);
 		sock.destroy();
 	});
 
-	sock.on('error', function() {
-		module.exports.emit('close');
-		console.log('The TCP socket to the Panasonic controller at IP: %s caused an error!', sock.remoteAddress);
+	sock.on('error', function(error) {
+		module.exports.emit(errorMessage, {ipSocket: sock, error});
+		module.exports.emit(closeMessage, sock);
 		sock.destroy();
 	});
 });
@@ -37,7 +87,7 @@ function setProgramTally(address, programTally){
 
 	const hexString = programTally.toString(16);
 
-	var message = new Buffer('.ATLY:80000000:00000000.');
+	const message = Buffer.from('.ATLY:80000000:00000000.');
 	message[0]  = 2;
 	message[23] = 3;
 	message.write(hexString, 14-hexString.length);
@@ -48,4 +98,12 @@ function setProgramTally(address, programTally){
 	});
 };
 
+module.exports = new events.EventEmitter();
 module.exports.setTally = setProgramTally;
+module.exports.eventTypes = {
+	raw: rawMessage,
+	sBus: sBusMessage,
+	connection: connectionMessage,
+	close: closeMessage,
+	error: errorMessage
+}
